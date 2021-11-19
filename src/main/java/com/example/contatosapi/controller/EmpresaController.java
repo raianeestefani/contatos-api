c186aaeab226e59d98758a7c0f7763089d637a17
raianeestefani <raianeestefanibm@gmail.com>
2021-11-19 19:01:30 -0300

    aula  19/11

--- src/main/java/com/example/contatosapi/controller/EmpresaController.java
+++ src/main/java/com/example/contatosapi/controller/EmpresaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -75,6 +76,12 @@ private ResponseEntity<List<Empresa>> findAll() {
  empresa = empresaService.save(empresa);
 	 
 	 return ResponseEntity.ok().body(empresa);
-	 
+	 }
+ 
+ @DeleteMapping(value = "{/id}")
+ public ResponseEntity<Void> delete (@PathVariable Long id){
+ empresaService.delete(id);
+ return ResponseEntity.noContent().build();
  }
+ 
 }
